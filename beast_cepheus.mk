4ee01f0a0d3801eb37fc0816c9123dbe4f162184
Akash <akashniki@gmail.com>
2019-07-21 10:35:45 +0200

    cepheus: Update build fingerprint to V10.2.15.0.PFAEUXM
    This fingerprint matches the vendor one and thus, passes safetynet :)
    
    Signed-off-by: mdeejay <mdjrussia@gmail.com>

--- beast_cepheus.mk
+++ beast_cepheus.mk
@@ -31,10 +31,10 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 BEAST_BUILD_TYPE := OFFICIAL
 
-BUILD_FINGERPRINT := "Xiaomi/cepheus/cepheus:9/PKQ1.181121.001/9.3.1:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/cepheus_eea/cepheus:9/PKQ1.181121.001/V10.2.15.0.PFAEUXM:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="cepheus-user 9 PKQ1.181121.001 9.3.1 release-keys" \
+    PRIVATE_BUILD_DESC="cepheus-user 9 PKQ1.181121.001 V10.2.15.0.PFAEUXM release-keys" \
     PRODUCT_NAME="cepheus" \
     TARGET_DEVICE="cepheus"
 
