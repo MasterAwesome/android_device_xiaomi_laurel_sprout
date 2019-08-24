f4c4530d35c31d75705b7554880cb94bf66f28b3
mdeejay <mdjrussia@gmail.com>
2019-08-24 07:58:04 +0200

    cepheus: Add system props from stock
    Signed-off-by: mdeejay <mdjrussia@gmail.com>

--- system-props.mk
+++ system-props.mk
@@ -3,11 +3,26 @@ PRODUCT_PROPERTY_OVERRIDES += \
     audio.deep_buffer.media=true \
     ro.af.client_heap_size_kbyte=7168 \
     ro.config.media_vol_steps=25 \
-    ro.config.vc_call_vol_steps=7
+    ro.config.vc_call_vol_steps=7 \
+    ro.qc.sdk.audio.ssr=false \
+    ro.qc.sdk.audio.fluencetype=none \
+    persist.audio.fluence.voicecall=true \
+    persist.audio.fluence.voicerec=false \
+    persist.audio.fluence.speaker=true \
+    tunnel.audio.encode=true \
+    audio.offload.buffer.size.kb=64 \
+    av.offload.enable=true \
+    use.voice.path.for.pcm.voip=true \
+    audio.offload.gapless.enabled=true
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.bluetooth.soc=cherokee
+    bt.max.hfpclient.connections=1 \
+    vendor.bluetooth.soc=cherokee \
+    ro.bluetooth.emb_wp_mode=false \
+    ro.bluetooth.wipower=false \
+    persist.vendor.btstack.enable.twsplus=true \
+    persist.vendor.btstack.connect.peer_earbud=true
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -28,6 +43,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.dpm.feature=1 \
     persist.vendor.dpm.loglevel=0 \
     persist.vendor.dpm.nsrm.bkg.evt=3955
+	
+# CoreSight
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.debug.coresight.config=stm-events
 
 # Data modules
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -56,18 +75,49 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.display.ad.hdr_calib_data=/vendor/etc/hdr_config.cfg \
     ro.vendor.display.ad.sdr_calib_data=/vendor/etc/sdr_config.cfg \
     ro.vendor.display.sensortype=2
+	
+# HWUI
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapsize=36m \
+    ro.hwui.texture_cache_size=72 \
+    ro.hwui.layer_cache_size=48 \
+    ro.hwui.r_buffer_cache_size=8 \
+    ro.hwui.path_cache_size=32 \
+    ro.hwui.gradient_cache_size=1 \
+    ro.hwui.drop_shadow_cache_size=6 \
+    ro.hwui.texture_cache_flushrate=0.4 \
+    ro.hwui.text_small_cache_width=2048 \
+    ro.hwui.text_small_cache_height=2048 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_large_cache_height=2048
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.disable_backpressure=1 \
     debug.sf.enable_hwc_vds=1 \
     sdm.debug.disable_inline_rotator=1 \
-    sdm.debug.disable_inline_rotator_secure=1
+    sdm.debug.disable_inline_rotator_secure=1 \
+    dev.pm.dyn_samplingrate=1 \
+    config.disable_rtt=true \
+    persist.sys.force_sw_gles=1
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
-    audio.offload.video=true \
-    media.settings.xml=/system/etc/media_profiles_vendor.xml
+    ro.hdmi.enable=true \
+    qcom.hw.aac.encoder=true \
+    media.stagefright.enable-player=true \
+    media.stagefright.enable-http=true \
+    media.stagefright.enable-aac=true \
+    media.stagefright.enable-qcp=true \
+    media.stagefright.enable-fma2dp=true \
+    media.stagefright.enable-scan=true \
+    mmp.enable.3g2=true \
+    media.aac_51_output_enabled=true \
+    mm.enable.smoothstreaming=true \
+    media.settings.xml=/system/etc/media_profiles_vendor.xml \
+    vendor.mm.enable.qcom_parser=63963135 \
+    persist.mm.enable.prefetch=true \
+    ro.netflix.bsp_rev=Q855-16947-1
 
 # Memory optimizations
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -75,7 +125,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Netflix custom property
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.netflix.bsp_rev=Q845-05000-1
+    ro.netflix.bsp_rev=Q855-16947-1
+	
+# NFC
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.nfc.port=I2C
+	
+# IMS
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.radio.VT_HYBRID_ENABLE=1 \
+    persist.vendor.data.iwlan.enable=true \
+    persist.radio.NO_STAPA=1
 
 # IOP
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -85,6 +147,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.perf.gestureflingboost.enable=true \
     vendor.perf.workloadclassifier.enable=true \
     ro.vendor.at_library=libqti-at.so
+	
+# Location
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.overlay.izat.optin=rro \
+    ro.location.osnlp.package=com.google.android.gms
 
 # RCS and IMS
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -102,13 +169,32 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ril.subscription.types=NV,RUIM \
     ro.telephony.default_network=22,22 \
     telephony.lteOnCdmaDevice=1
+	
+# Sensors
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.sensors.enable.mag_filter=true
 
 # SSR
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.ssr.enable_ramdumps=0 \
     persist.vendor.ssr.restart_level=ALL_ENABLE
+	
+# Strict Mode
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.strictmode.disable=true
 
 # WFD
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.wfd.enable=1 \
-    persist.sys.wfd.virtual=0
+    persist.sys.wfd.virtual=0 \
+    debug.sf.enable_hwc_vds=1
+	
+# WLAN
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.wlan.vendor=qcom \
+    ro.wlan.chip=39xx \
+    ro.wlan.mimo=1
+	
+# ZRAM
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.config.zram=false
