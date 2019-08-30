05219cf7dd92dc315e9e11b8b74bfc0855353101
mdeejay <mdjrussia@gmail.com>
2019-08-30 19:26:46 +0200

    lauel_sprout: Update build fingerprint
    Signed-off-by: mdeejay <mdjrussia@gmail.com>

--- beast_laurel_sprout.mk
+++ beast_laurel_sprout.mk
@@ -32,11 +32,11 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 BEAST_BUILD_TYPE := OFFICIAL
 
-BUILD_FINGERPRINT := "Xiaomi/laurel_sprout_eea/laurel_sprout:9/PKQ1.190416.001/V10.3.5.0.PFQEUXM:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/laurel_sprout/laurel_sprout:9/PKQ1.190416.001/V10.3.4.0.PFQMIXM:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="laurel_sprout-user 9 PKQ1.190416.001 V10.3.5.0.PFQEUXM release-keys" \
-    PRODUCT_NAME="cepheus" \
-    TARGET_DEVICE="cepheus"
+    PRIVATE_BUILD_DESC="laurel_sprout-user 9 PKQ1.190416.001 V10.3.4.0.PFQMIXM release-keys" \
+    PRODUCT_NAME="laurel_sprout" \
+    TARGET_DEVICE="laurel_sprout"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
