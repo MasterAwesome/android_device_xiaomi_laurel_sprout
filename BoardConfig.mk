d3af5b5ad6eed00560d849c222bd8d669721e314
SyberHexen <SyberHexen@gmail.com>
2019-09-01 13:00:30 -0700

    laurel_sprout: declare v1 boot header
    - per documentation here: https://source.android.com/devices/bootloader/boot-image-header
    * All devices launching with Android 9 must use a boot header version of 1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,6 +48,8 @@ BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_PHONY_TARGETS := true
 
 # Kernel
+BOARD_BOOT_HEADER_VERSION := 1
+BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 lpm_levels.sleep_disabled=1 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 firmware_class.path=/vendor/firmware_mnt/image earlycon=msm_geni_serial,0x4a90000 loop.max_part=7 cgroup.memory=nokmem,nosocket androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += androidboot.vbmeta.avb_version=1.0
