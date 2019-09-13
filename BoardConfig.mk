28c9aa5e0f291a6847d1547db1527fd638f71353
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-09-13 14:43:24 +0200

    laurel_sprout: Dexpreopt everything
    Signed-off-by: mdeejay <mdjrussia@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,7 +86,6 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
     WITH_DEXPREOPT ?= true
-    WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
   endif
 endif
 
