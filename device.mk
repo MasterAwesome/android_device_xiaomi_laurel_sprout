8a582fceaab3a1ffbdebc92e2476338a9d9bc859
LuK1337 <priv.luk@gmail.com>
2019-09-25 14:06:56 +0200

    Let system read and write to FRP block device
    * No clue why this is needed but ok.
    
    Change-Id: Ib40ab18e42ced8995f57c9d0a8e1a6decdc7f146
    
    Signed-off-by: mdeejay <mdjrussia@gmail.com>

--- device.mk
+++ device.mk
@@ -129,7 +129,8 @@ PRODUCT_COPY_FILES += \
 # Init
 PRODUCT_PACKAGES += \
     init.qcom.rc \
-    init.recovery.qcom.rc
+    init.recovery.qcom.rc \
+    ueventd.qcom.rc
 	
 # IMS
 PRODUCT_PACKAGES += \
