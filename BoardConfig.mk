31dba88bc739ad279408cf3eadc7b1378e198557
theimpulson <aayushgupta219@gmail.com>
2019-11-16 06:02:40 +0000

    laurel_sprout: Build lineage's device system sepolicy for QCOM devices
    Change-Id: I846cc73d07412950d26c06038391ade5df65fe07

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -150,7 +150,8 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/qcom/sepolicy/generic/private \
-    $(DEVICE_PATH)/sepolicy/qcom-qva-private
+    $(DEVICE_PATH)/sepolicy/qcom-qva-private \
+    device/lineage/sepolicy/qcom/system
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
     device/qcom/sepolicy/generic/public \
