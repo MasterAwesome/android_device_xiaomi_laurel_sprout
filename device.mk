3908d4d83205dba038bdc62fd484223a123ed6df
DennySPB <dennyspb@gmail.com>
2020-01-17 17:34:37 +0300

    Include google/vendor/customization
    for google play apex updates
    
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: Ie7822add773a372a2df974e003d11380821c2c60

--- device.mk
+++ device.mk
@@ -17,6 +17,7 @@
 
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/laurel_sprout/laurel_sprout-vendor.mk)
+$(call inherit-product, vendor/google/customization/config.mk)
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
