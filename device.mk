bd4d6c0a9bf408b9123a0fd1401306ebb7a70f94
dianlujitao <dianlujitao@lineageos.org>
2020-02-06 23:02:18 +0800

    laurel_sprout: Improve lights HAL
    completely overwrite lights and import from HarukeyUA's tree
    original commit message:
    
     - Based off LineageOS/android_device_xiaomi_sdm660-common
    
     * Use libbase logging and file utils
     * Redo the way we read max brightness from sysfs
     * Convert constants to constexpr
     * Move helper functions to anonymous namespace
     * Bug fixes
     * Start the service after permissions are set up because it reads max
       brightness in constructor.
    
     - Pig: Adapt for our 4.14 shiny qti tri led.
    
    Change-Id: I145d393c785e182060c5651e796d489f8c4d697b
    Signed-off-by: HarukeyUA <inazarrusnak@gmail.com>

--- device.mk
+++ device.mk
@@ -185,7 +185,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_trinket
+    android.hardware.light@2.0-service.laurel_sprout
 
 # Media
 PRODUCT_COPY_FILES += \
