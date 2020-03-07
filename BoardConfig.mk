ea540cdda95a3936742f4d5c1ee43df525444801
HarukeyUA <inazarrusnak@gmail.com>
2020-03-07 15:57:38 +0200

    laurel_sprout: Disable usage of binutils
    https://github.com/syberia-project/platform_vendor_syberia/commit/81ee30e7df1048b912899a236c534ab1e2097747

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,6 +62,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := vendor/laurel_sprout-perf_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/laurel_sprout
+TARGET_KERNEL_NO_LLVM_BINUTILS := true
 
 # Platform
 TARGET_BOARD_PLATFORM := trinket
