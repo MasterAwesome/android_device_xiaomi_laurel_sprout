9d361b61e94f994543b262c1494553fba9a4925f
prakash <prakaship78@gmail.com>
2020-03-19 17:14:31 +0100

    Copy FOD permission from Lineage vendor

--- device.mk
+++ device.mk
@@ -133,6 +133,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_trinket
 
+PRODUCT_COPY_FILES += \
+    vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:system/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
