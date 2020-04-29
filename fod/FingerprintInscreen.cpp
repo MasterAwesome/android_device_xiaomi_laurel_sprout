51dc62923216b1d207423570395371ad9e8db71c
Golbinex <2061409-Golbinex@users.noreply.gitlab.com>
2020-04-29 22:15:12 +0200

    Revert "Revert "laurel_sprout: fod: Calculate dim amount based on real brightness""
    This reverts commit 3c9fcc5ea6a91a6802802ce6ff93a95667e320a6.

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -40,7 +40,17 @@
 #define FOD_SENSOR_Y 1356
 #define FOD_SENSOR_SIZE 134
 
+#define BRIGHTNESS_PATH "/sys/class/backlight/panel0-backlight/brightness"
+
 namespace {
+
+template <typename T>
+static T get(const std::string& path, const T& def) {
+    std::ifstream file(path);
+    T result;
+    file >> result;
+    return file.fail() ? def : result;
+}
 
 template <typename T>
 static void set(const std::string& path, const T& value) {
@@ -118,13 +128,14 @@ Return<void> FingerprintInscreen::setLongPressEnabled(bool) {
     return Void();
 }
 
-Return<int32_t> FingerprintInscreen::getDimAmount(int32_t brightness) {
+Return<int32_t> FingerprintInscreen::getDimAmount(int32_t /* brightness */) {
+    int realBrightness = get(BRIGHTNESS_PATH, 0);
     float alpha;
 
-    if (brightness > 62.0) {
-        alpha = 1.0 - pow((((brightness / 255.0) * 430.0) / 600.0), 0.455);
+    if (realBrightness > 500) {
+        alpha = 1.0 - pow(realBrightness / 2047.0 * 430.0 / 600.0, 0.455);
     } else {
-        alpha = 1.0 - pow((brightness / 210.0), 0.455);
+        alpha = 1.0 - pow(realBrightness / 1680.0, 0.455);
     }
 
     return 255 * alpha;
