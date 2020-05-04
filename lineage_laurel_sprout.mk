36e216c106fe2df5ae430c90638a0b5a90c8520e
Kuba Wojciechowski <nullbytepl@gmail.com>
2020-05-04 19:41:16 +0000

    laurel_sprout: Update real fingerprint to 11.0.14.0
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>

--- lineage_laurel_sprout.mk
+++ lineage_laurel_sprout.mk
@@ -31,10 +31,10 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A3
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/laurel_sprout/laurel_sprout:10/QKQ1.190910.002/V11.0.7.0.QFQMIXM:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/laurel_sprout/laurel_sprout:10/QKQ1.190910.002/V11.0.14.0.QFQMIXM:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="qssi-user 10 QKQ1.190910.002 V11.0.7.0.QFQMIXM release-keys" \
+    PRIVATE_BUILD_DESC="qssi-user 10 QKQ1.190910.002 V11.0.14.0.QFQMIXM release-keys" \
     PRODUCT_NAME="laurel_sprout" \
     TARGET_DEVICE="laurel_sprout"
 
