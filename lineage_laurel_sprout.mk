db2438951e8f6224f0bd1a149497bf8a1c922cb9
Charles-IV <charles@charles-iv.uk>
2020-05-26 12:35:11 +0100

    laurel_sprout: little cleanup

--- lineage_laurel_sprout.mk
+++ lineage_laurel_sprout.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common Syberia stuff
+# Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 720
