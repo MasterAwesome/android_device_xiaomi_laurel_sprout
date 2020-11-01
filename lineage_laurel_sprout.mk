26a38be3be3332891eaae85545e74c81c3bfa671
Arvind Mukund <armu30@gmail.com>
2020-10-31 20:58:57 -0400

    obtain_vendor.sh: Sat Oct 31 08:58:57 PM EDT 2020
    Signed-off-by: Arvind Mukund <armu30@gmail.com>
    Change-Id: I0507612fe382c5a8d33265122ae1ef4b4885d053

--- lineage_laurel_sprout.mk
+++ lineage_laurel_sprout.mk
@@ -33,10 +33,10 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A3
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/laurel_sprout_eea/laurel_sprout:10/QKQ1.190910.002/V11.0.8.0.QFQEUXM:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/laurel_sprout/laurel_sprout:10/QKQ1.190910.002/V11.0.22.0.QFQMIXM:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="qssi-user 10 QKQ1.190910.002 V11.0.8.0.QFQEUXM release-keys" \
+    PRIVATE_BUILD_DESC="qssi-user 10 QKQ1.190910.002 V11.0.22.0.QFQMIXM release-keys" \
     PRODUCT_NAME="laurel_sprout"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
