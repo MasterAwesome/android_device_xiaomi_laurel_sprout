0bea36834a66eef05f735fc1c70a797cff4c8734
Arvind Mukund <armu30@gmail.com>
2021-01-31 14:26:44 -0500

    We have vndk version 30. We don't need this anymore
    Signed-off-by: Arvind Mukund <armu30@gmail.com>

--- device.mk
+++ device.mk
@@ -132,12 +132,8 @@ PRODUCT_COPY_FILES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.manager@1.0 \
     libhidltransport \
-    libhwbinder \
-    libhidltransport.vendor \
-    libhwbinder.vendor
+    libhwbinder
 
 # Init
 PRODUCT_PACKAGES += \
