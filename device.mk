6dfe761d968c6643f18d5122f179b70979629686
Arvind Mukund <armu30@gmail.com>
2021-02-18 19:20:41 -0500

    Revert "include system.prop from V12.0.3.0.RFQMIXM"
    This reverts commit 643e93965fff92f1504dfe5d83ec399035001d84.
    
    Change-Id: I04a7fa795dbe9c6572559c382809e144ce015f22

--- device.mk
+++ device.mk
@@ -65,6 +65,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
 
+# Properties
+-include $(LOCAL_PATH)/system_prop.mk
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
