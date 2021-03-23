169d24de6abeb9c76e528731c54c26c4f8ff2bba
Arvind Mukund <armu30@gmail.com>
2021-03-23 17:21:50 -0400

    audio: a2dp bt now works as of v12.0.5.0
    Signed-off-by: Arvind Mukund <armu30@gmail.com>
    Change-Id: I865923a83a80d07b4eca594a12327befcb5787eb

--- init/init_laurel_sprout.cpp
+++ init/init_laurel_sprout.cpp
@@ -52,10 +52,5 @@ void vendor_load_properties()
 {
     property_override("ro.control_privapp_permissions", "log");
     property_override("ro.oem_unlock_supported", "0");
-    property_override("ro.bluetooth.a2dp_offload.supported", "false");
-    property_override("persist.bluetooth.a2dp_offload.disabled", "true");
-    property_override("persist.bluetooth.bluetooth_audio_hal.disabled", "true");
-    property_override("persist.vendor.qcom.bluetooth.enable.splita2dp", "false");
-    property_override("vendor.audio.feature.a2dp_offload.enable", "false");
     property_override("ro.product.model", "MA-A3");  // Get it? MA? MasterAwesome?? Also passes safetynet.
 }
