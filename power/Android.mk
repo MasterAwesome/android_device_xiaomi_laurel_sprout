8e5efb4130e67a78013405f634367716a03f9ec1
Arvind Mukund <armu30@gmail.com>
2022-01-31 11:14:13 -0800

    power: LA.UM.9.11.r1-05200-NICOBAR.QSSI12.0 update
    Signed-off-by: Arvind Mukund <armu30@gmail.com>
    Change-Id: I9d09a41d0111a2112af72a7c71dda2ff58dea47e

--- power/Android.mk
+++ power/Android.mk
@@ -4,26 +4,18 @@ ifeq ($(call is-vendor-board-platform,QCOM),true)
 
 include $(CLEAR_VARS)
 
-LOCAL_SHARED_LIBRARIES := \
-    liblog \
-    libcutils \
-    libdl \
-    libbase \
-    libutils \
-    android.hardware.power-ndk_platform \
-    libbinder_ndk
+ifneq ( ,$(filter 11 R ,$(PLATFORM_VERSION)))
+LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2 libbase libutils android.hardware.power-ndk_platform libbinder_ndk
+else
+LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2 libbase libutils android.hardware.power-V1-ndk_platform libbinder_ndk
+endif
 
-LOCAL_HEADER_LIBRARIES := \
-    libhardware_headers
+LOCAL_SRC_FILES := power-common.c metadata-parser.c utils.c list.c hint-data.c powerhintparser.c Power.cpp main.cpp
 
-LOCAL_SRC_FILES := \
-    power-common.c \
-    metadata-parser.c \
-    utils.c \
-    list.c \
-    hint-data.c \
-    Power.cpp \
-    main.cpp \
+LOCAL_HEADER_LIBRARIES += libutils_headers
+LOCAL_HEADER_LIBRARIES += libhardware_headers
+LOCAL_C_INCLUDES := external/libxml2/include \
+                    external/icu/icu4c/source/common
 
 LOCAL_CFLAGS += -Wall -Wextra -Werror
 
